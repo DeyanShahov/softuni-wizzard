d8454d9e85e37009af7e0a23d307bc60617007ab
DeyanShahov <redfoxbg@gmail.com>
2021-07-03 15:45:43 +0300

    Add wizzard up movement

--- gameState.js
+++ gameState.js
@@ -3,7 +3,12 @@ function initGameState(){
         gameOver: false,
         wizzard: {
             x: 100,
-            y: 200
+            y: 200,
+            speed: 5,
+        },
+        keys:{},
+        config: {
+
         }
     };
 
