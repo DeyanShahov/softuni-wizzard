da00a272e11f536b666509f70761b7b9b43d4720
DeyanShahov <redfoxbg@gmail.com>
2021-07-03 14:37:26 +0300

    Add wizzard on start game

--- init.js
+++ init.js
@@ -4,11 +4,26 @@ function gameFactory(){
     let startScreen = document.querySelector('.start-screen');
     let playScreen = document.querySelector('.play-screen');
 
+    let wizzardElement = createWizzard(100, 200);
+
+    playScreen.appendChild(wizzardElement);
+
     Object.defineProperties(factory, {
         startScreen: { get: () => startScreen},
-        playScreen: { get: () => playScreen}
+        playScreen: { get: () => playScreen},
+        wizzardElement,//: { get: () => wizzardElement},
     });
 
     return factory;
 
+}
+
+function createWizzard(posX, posY){
+    let wizzardElement = document.createElement('div');
+
+    wizzardElement.classList.add('wizzard');
+    wizzardElement.style.top = posY + 'px';
+    wizzardElement.style.left = posX + 'px';
+
+    return wizzardElement;
 }
